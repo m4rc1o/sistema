eaf9988300147dae415f4d5c28dd5564b0abcdaf
m4rc10 <marcioinaciosantana@gmail.com>
2018-10-25 23:43:55 -0300

    Adicionando o atributo Aramo4

--- A.java
+++ A.java
@@ -2,6 +2,15 @@ public class A{
 	private int A1;
 	private float A2;
         private String A3;
+        private String Aramo4;
+
+    public String getAramo4() {
+        return Aramo4;
+    }
+
+    public void setAramo4(String Aramo4) {
+        this.Aramo4 = Aramo4;
+    }
 
     public float soma(){
         return this.A1 + this.A2;
